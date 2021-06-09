ff13b820cd7679c580f4257b166d7bd0eb69c5ea
Vicente Herrera <vicenteherrera@vicenteherrera.com>
2021-06-09 11:46:39 +0200

    Updated box1 to latest Falco requirements

--- box2/install_falco.sh
+++ box2/install_falco.sh
@@ -3,19 +3,24 @@
 sudo apt-get update
 
 # Falco install prerequisites
-sudo apt-get install -y curl gnupg2
+sudo apt-get install -y curl gnupg2 apt-transport-https ca-certificates
 
 # Falco deb source
 curl -s https://falco.org/repo/falcosecurity-3672BA8F.asc | sudo apt-key add -
-echo "deb https://dl.bintray.com/falcosecurity/deb stable main" | sudo tee -a /etc/apt/sources.list.d/falcosecurity.list
+echo "deb https://download.falco.org/packages/deb stable main" | sudo tee -a /etc/apt/sources.list.d/falcosecurity.list
 sudo apt-get update -y
 
 # Kernel headers
+# This is faster, doesn't detect all kinds of headers
 sudo apt-get -y install linux-headers-$(uname -r)
+# This is slower, but works always on amd64
+apt-get install linux-image-amd64 linux-headers-amd64
 
 # Install Falco
 sudo apt-get install -y falco
 
+
+# Uncomment to install Apache webserver and link web folder
 
 # apt-get install -y apache2
 # if ! [ -L /var/www ]; then
---
+++ box2/run_event_generator.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker run -it --rm falcosecurity/event-generator run syscall --loop
+
